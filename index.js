047e98245888cb020c058636ff5111da276460ae
DanielBrandao12 <drb12da@gmail.com>
2022-09-28 14:14:48 -0300

    commit inicial

--- index.js
+++ index.js
@@ -2,16 +2,7 @@
 const linhaNome = document.getElementById('linhaNome')
 const spanNome = document.getElementById('spanNome')
 
-const botao = document.getElementById('btn')
 
-
-botao.addEventListener('click', e=>{
-    if(nomeCompleto.value == ''){
-        spanNome.innerText ='Preencha o campo'
-        spanNome.style.color = 'red'
-        linhaNome.style.background ='red'
-        
-    }
   
 })
 
@@ -29,6 +20,7 @@ nomeCompleto.addEventListener('input',e =>{
 
 })*/
 
+const botao = document.getElementById('btn')
 
 const senha = document.getElementById('password')
 const spanSenha = document.getElementById('spanSenha')
@@ -39,10 +31,41 @@ senha.addEventListener('input', e => {
     const inputLength = e.target.value.length;
     const charsLeft = 12 - inputLength;
 
+
     if(charsLeft > 0){
         spanSenha.innerText ='Mínimo 12 caracteres'
-        spanSenha.style.color='gray'
-        linhaSenha.style.background ='gray'
+        spanSenha.style.color='red'
+        linhaSenha.style.background ='red'
         return
     }
+    if (inputLength >= 12 && inputLength <= 15) {
+        spanSenha.innerText = "Quantidade OK";
+        spanSenha.style.color = "#146fd6";
+        linhaSenha.style.background = "#146fd6";
+        return;
+        
+ 
+      }
+    
+    
+      if (inputLength >= 16 && inputLength <= 18) {
+        spanSenha.innerText = "Muito bom";
+        spanSenha.style.color = "#1e5731";
+        linhaSenha.style.background = "#1e5731";
+        return;
+      }
+    
+     
+      if (inputLength >= 19) {
+        spanSenha.innerText = "Perfeito";
+        spanSenha.style.color = "#00b45d";
+        linhaSenha.style.background = "#00b45d";
+        return;
+      }
+
+})
+
+botao.addEventListener('click', e=>{
+
+
 })
